5dc2921b3a53c8265f668aa281d4db8b143e36f9
crodriguez <crodriguez@hexagonconsulting.net>
2023-03-21 11:59:13 -0300

    init app

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["hexathon/emotionsreport/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"hexathon/emotionsreport/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
